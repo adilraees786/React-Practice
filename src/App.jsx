1450219993bb5e6698a34bbe0abac0d63e10fe26
adilraees786 <adilraees911@gmail.com>
2025-01-02 12:27:37 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-export default function practice(){
-  return(
-    <div>
-      <h1>Practice</h1>
-    </div>
+// export default function practice(){
+//   return(
+//     <div className=" h-10 w-10 bg-black">
+//       <h1 className="  text-red-700"> React Practice </h1>
+//     </div>
+//   )
+// }// src/App.jsx
+
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
